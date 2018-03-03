4affd31544deaa4453a72822f17dbd3941ede4ce
ealvarez <emilioa84@hotmail.com>
2018-03-02 20:33:50 -0800

    Fixed Search for Caption

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function myFunction() {
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (a.getAttribute("data-fresco-caption").toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
